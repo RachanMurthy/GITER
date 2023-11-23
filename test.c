25b4b0bc38b61be4a689613e81aa7a07739f6798
rachan murthy <rachan3thi@gmail.com>
2023-11-23 09:23:34 +0530

    chaitanya

--- test.c
+++ test.c
@@ -657,154 +657,323 @@
 //     return 0;
 // }
 
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-typedef struct NODE Node;
-struct NODE {
-    char usn[20];
-    char name[20];
-    char branch[20];
-    char sem[20];
-    char phone[20];
-    Node *next;
+// typedef struct NODE Node;
+// struct NODE {
+//     char usn[20];
+//     char name[20];
+//     char branch[20];
+//     char sem[20];
+//     char phone[20];
+//     Node *next;
+// };
+
+// Node *start = NULL;
+// int flag = 0; /* is 1 when list is created */
+
+// void get(char *prompt, char *s) {
+//     printf("Enter %s: ", prompt);
+//     scanf("%19s", s); // Limiting input to avoid buffer overflow
+// }
+
+// Node *info() {
+//     Node *s;
+//     s = malloc(sizeof(Node));
+//     if (s == NULL) {
+//         fprintf(stderr, "Memory allocation failed!\n");
+//         exit(1);
+//     }
+//     get("USN", s->usn);
+//     get("Name", s->name);
+//     get("Branch", s->branch);
+//     get("Semester", s->sem);
+//     get("Phone", s->phone);
+//     s->next = NULL;
+//     return s;
+// }
+
+// void insert_front() {
+//     Node *student = info();
+//     student->next = start;
+//     start = student;
+//     flag = 1;
+// }
+
+// void insert_end() {
+//     Node *student = info();
+//     if (start == NULL) {
+//         start = student;
+//     } else {
+//         Node *p = start;
+//         while (p->next != NULL) {
+//             p = p->next;
+//         }
+//         p->next = student;
+//     }
+//     flag = 1;
+// }
+
+// void delete_front() {
+//     if (start == NULL) {
+//         printf("The list is empty. \n");
+//         return;
+//     }
+//     Node *p = start;
+//     start = start->next;
+//     printf("Deleted Student USN is %s \n\n", p->usn);
+//     free(p);
+// }
+
+// void delete_end() {
+//     if (start == NULL) {
+//         printf("The list is empty. \n");
+//         return;
+//     }
+//     if (start->next == NULL) {
+//         printf("Deleted Student USN is %s \n\n", start->usn);
+//         free(start);
+//         start = NULL;
+//         return;
+//     }
+//     Node *p = start, *q = NULL;
+//     while (p->next != NULL) {
+//         q = p;
+//         p = p->next;
+//     }
+//     q->next = NULL;
+//     printf("Deleted Student USN is %s \n\n", p->usn);
+//     free(p);
+// }
+
+// void display() {
+//     if (start == NULL) {
+//         printf("The list is empty.\n");
+//         return;
+//     }
+//     Node *p = start;
+//     int n = 1;
+//     while (p != NULL) {
+//         printf("%d. %s %s %s %s %s\n", n, p->usn, p->name, p->branch, p->sem, p->phone);
+//         p = p->next;
+//         n++;
+//     }
+// }
+
+// void create() {
+//     int n;
+//     printf("Enter number of students: ");
+//     scanf("%d", &n);
+//     for (int i = 1; i <= n; i++) {
+//         printf("\nStudent %d details\n", i);
+//         insert_front();
+//     }
+// }
+
+// void exit_program() {
+//     exit(0);
+// }
+
+// int main() {
+//     int choice;
+//     struct MENU {
+//         char *name;
+//         void (*func)();
+//     } menu[] = {
+//         {"Done", exit_program},
+//         {"Create", create},
+//         {"Insert front", insert_front},
+//         {"Insert end", insert_end},
+//         {"Delete front", delete_front},
+//         {"Delete end", delete_end},
+//         {"Display", display}
+//     };
+
+//     int size = sizeof(menu) / sizeof(menu[0]);
+//     while (1) {
+//         printf("\n-------SLL Menu-------\n");
+//         for (int i = 1; i < size; i++) {
+//             printf("%d. %s\n", i, menu[i].name);
+//         }
+//         printf("---------------------------------\n");
+//         printf("Enter your choice: ");
+//         scanf("%d", &choice);
+//         if (choice < 1 || choice >= size) {
+//             printf("Invalid choice. Please try again.\n");
+//             continue;
+//         }
+//         menu[choice].func();
+//     }
+//     return 0;
+// }
+
+#include<stdio.h> #include<stdlib.h> struct node
+{
+struct node *prev; struct node *next; int data;
 };
-
-Node *start = NULL;
-int flag = 0; /* is 1 when list is created */
-
-void get(char *prompt, char *s) {
-    printf("Enter %s: ", prompt);
-    scanf("%19s", s); // Limiting input to avoid buffer overflow
+struct node *head;
+void insertion_beginning(); void insertion_last();
+void insertion_specified(); void deletion_beginning(); void deletion_last();
+void deletion_specified(); void display();
+void search();
+void main ()
+{
+int choice =0;
+while(choice != 9) {
+printf("\n*********Main Menu*********\n");
+printf("\nChoose one option from the following list ...\n"); printf("\n===============================================\n"); printf("\n1.Insert in begining\n2.Insert at last\n3.Insert at any random location\n4.Delete
+from Beginning\n
+5.Delete from last\n6.Delete the node after the given data\n7.Search\n8.Show\n9.Exit\n"); printf("\nEnter your choice?\n");
+scanf("\n%d",&choice);
+switch(choice)
+{
+case 1: insertion_beginning(); break;
+case 2:
+insertion_last(); break;
+case 3: insertion_specified(); break;
+case 4: deletion_beginning(); break;
+case 5: deletion_last();
+   break;
+case 6:
+deletion_specified();
+break;
+case 7:
+search();
+break;
+case 8:
+display();
+break;
+case 9:
+exit(0);
+break;
+default:
+printf("Please enter valid choice..");
+} }
 }
-
-Node *info() {
-    Node *s;
-    s = malloc(sizeof(Node));
-    if (s == NULL) {
-        fprintf(stderr, "Memory allocation failed!\n");
-        exit(1);
-    }
-    get("USN", s->usn);
-    get("Name", s->name);
-    get("Branch", s->branch);
-    get("Semester", s->sem);
-    get("Phone", s->phone);
-    s->next = NULL;
-    return s;
+void insertion_beginning() {
+struct node *ptr;
+int item;
+ptr = (struct node *)malloc(sizeof(struct node)); if(ptr == NULL)
+{
+printf("\nOVERFLOW"); }
+else {
+printf("\nEnter Item value"); scanf("%d",&item);
+if(head==NULL) {
+ptr->next = NULL; ptr->prev=NULL; ptr->data=item; head=ptr;
+} else {
+ptr->data=item; ptr->prev=NULL; ptr->next = head; head->prev=ptr; head=ptr;
 }
-
-void insert_front() {
-    Node *student = info();
-    student->next = start;
-    start = student;
-    flag = 1;
+ 
+   printf("\nNode inserted\n"); }
 }
-
-void insert_end() {
-    Node *student = info();
-    if (start == NULL) {
-        start = student;
-    } else {
-        Node *p = start;
-        while (p->next != NULL) {
-            p = p->next;
-        }
-        p->next = student;
-    }
-    flag = 1;
+void insertion_last() {
+struct node *ptr,*temp;
+int item;
+ptr = (struct node *) malloc(sizeof(struct node)); if(ptr == NULL)
+{
+printf("\nOVERFLOW"); }
+else {
+printf("\nEnter value"); scanf("%d",&item);
+ptr->data=item; if(head == NULL) {
+ptr->next = NULL; ptr->prev = NULL; head = ptr;
+} else {
+temp = head; while(temp->next!=NULL) {
+temp = temp->next; }
+temp->next = ptr; ptr ->prev=temp; ptr->next = NULL; }
 }
-
-void delete_front() {
-    if (start == NULL) {
-        printf("The list is empty. \n");
-        return;
-    }
-    Node *p = start;
-    start = start->next;
-    printf("Deleted Student USN is %s \n\n", p->usn);
-    free(p);
+printf("\nnode inserted\n");
 }
-
-void delete_end() {
-    if (start == NULL) {
-        printf("The list is empty. \n");
-        return;
-    }
-    if (start->next == NULL) {
-        printf("Deleted Student USN is %s \n\n", start->usn);
-        free(start);
-        start = NULL;
-        return;
-    }
-    Node *p = start, *q = NULL;
-    while (p->next != NULL) {
-        q = p;
-        p = p->next;
-    }
-    q->next = NULL;
-    printf("Deleted Student USN is %s \n\n", p->usn);
-    free(p);
+void insertion_specified() {
+struct node *ptr,*temp;
+int item,loc,i;
+ptr = (struct node *)malloc(sizeof(struct node)); if(ptr == NULL)
+{
+printf("\n OVERFLOW");
+ 
+   } else {
+temp=head;
+printf("Enter the location"); scanf("%d",&loc); for(i=0;i<loc;i++)
+{
+temp = temp->next; if(temp == NULL) {
+printf("\n There are less than %d elements", loc);
+return; }
 }
-
+printf("Enter value"); scanf("%d",&item); ptr->data = item;
+ptr->next = temp->next; ptr -> prev = temp; temp->next = ptr; temp->next->prev=ptr; printf("\nnode inserted\n");
+} }
+void deletion_beginning() {
+struct node *ptr; if(head == NULL) {
+printf("\n UNDERFLOW"); }
+else if(head->next == NULL) {
+head = NULL;
+free(head);
+printf("\nnode deleted\n");
+} else {
+ptr = head;
+head = head -> next;
+head -> prev = NULL; free(ptr);
+printf("\nnode deleted\n");
+}
+ 
+   }
+void deletion_last() {
+struct node *ptr; if(head == NULL) {
+printf("\n UNDERFLOW"); }
+else if(head->next == NULL) {
+head = NULL;
+free(head);
+printf("\nnode deleted\n");
+} else {
+ptr = head; if(ptr->next != NULL) {
+ptr = ptr -> next; }
+ptr -> prev -> next = NULL; free(ptr);
+printf("\nnode deleted\n");
+} }
+void deletion_specified() {
+struct node *ptr, *temp;
+int val;
+printf("\n Enter the data after which the node is to be deleted : "); scanf("%d", &val);
+ptr = head;
+while(ptr -> data != val)
+ptr = ptr -> next;
+if(ptr -> next == NULL)
+{
+printf("\nCan't delete\n"); }
+else if(ptr -> next -> next == NULL) {
+ptr ->next = NULL; }
+else {
+temp = ptr -> next;
+ptr -> next = temp -> next;
+ 
+   temp -> next -> prev = ptr; free(temp);
+printf("\nnode deleted\n");
+} }
 void display() {
-    if (start == NULL) {
-        printf("The list is empty.\n");
-        return;
-    }
-    Node *p = start;
-    int n = 1;
-    while (p != NULL) {
-        printf("%d. %s %s %s %s %s\n", n, p->usn, p->name, p->branch, p->sem, p->phone);
-        p = p->next;
-        n++;
-    }
+struct node *ptr;
+printf("\n printing values...\n"); ptr = head;
+while(ptr != NULL)
+{
+printf("%d\n",ptr->data);
+ptr=ptr->next; }
 }
-
-void create() {
-    int n;
-    printf("Enter number of students: ");
-    scanf("%d", &n);
-    for (int i = 1; i <= n; i++) {
-        printf("\nStudent %d details\n", i);
-        insert_front();
-    }
+void search() {
+struct node *ptr; int item,i=0,flag; ptr = head;
+if(ptr == NULL) {
+printf("\nEmpty List\n"); }
+else {
+printf("\nEnter item which you want to search?\n"); scanf("%d",&item);
+while (ptr!=NULL)
+{
+if(ptr->data == item) {
+printf("\nitem found at location %d ",i+1); flag=0;
+break;
+} else {
+flag=1; }
+i++;
+ptr = ptr -> next; }
+if(flag==1) {
+printf("\nItem not found\n");
+ 
+} }
 }
-
-void exit_program() {
-    exit(0);
-}
-
-int main() {
-    int choice;
-    struct MENU {
-        char *name;
-        void (*func)();
-    } menu[] = {
-        {"Done", exit_program},
-        {"Create", create},
-        {"Insert front", insert_front},
-        {"Insert end", insert_end},
-        {"Delete front", delete_front},
-        {"Delete end", delete_end},
-        {"Display", display}
-    };
-
-    int size = sizeof(menu) / sizeof(menu[0]);
-    while (1) {
-        printf("\n-------SLL Menu-------\n");
-        for (int i = 1; i < size; i++) {
-            printf("%d. %s\n", i, menu[i].name);
-        }
-        printf("---------------------------------\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-        if (choice < 1 || choice >= size) {
-            printf("Invalid choice. Please try again.\n");
-            continue;
-        }
-        menu[choice].func();
-    }
-    return 0;
-}
-
